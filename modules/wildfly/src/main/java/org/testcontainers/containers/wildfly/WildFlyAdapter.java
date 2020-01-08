8692b234b8bdb6b7d069aa66cc6841fcfa183d84
Philip Riecks <mail@philipriecks.de>
2020-01-08 08:19:49 +0100

    Introduce WildFly module
    Signed-off-by: Philip Riecks <mail@philipriecks.de>

---
+++ modules/wildfly/src/main/java/org/testcontainers/containers/wildfly/WildFlyAdapter.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2019 IBM Corporation and others
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.testcontainers.containers.wildfly;
+
+import org.microshed.testing.testcontainers.spi.ServerAdapter;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+
+import java.io.File;
+import java.util.Optional;
+
+public class WildFlyAdapter implements ServerAdapter {
+
+    @Override
+    public int getPriority() {
+        return PRIORITY_RUNTIME_MODULE;
+    }
+
+    @Override
+    public int getDefaultHttpPort() {
+        return 8080;
+    }
+
+    @Override
+    public int getDefaultHttpsPort() {
+        return 8443;
+    }
+
+    @Override
+    public ImageFromDockerfile getDefaultImage(File appFile) {
+        String appName = appFile.getName();
+        // Compose a docker image equivalent to doing:
+        // FROM jboss/wildfly:18.0.1.Final
+        // ADD target/myservice.war /opt/jboss/wildfly/standalone/deployments
+        ImageFromDockerfile image = new ImageFromDockerfile()
+                .withDockerfileFromBuilder(builder -> builder.from("jboss/wildfly:18.0.1.Final")
+                        .add(appName, "/opt/jboss/wildfly/standalone/deployments")
+                        .build())
+                .withFileFromFile(appName, appFile);
+        return image;
+
+    }
+
+    @Override
+    public Optional<String> getReadinessPath() {
+        return Optional.of("/health");
+    }
+}
