f7a5b78ae21d7542d7bce1e2649e38b7a34beaaa
asjervanasten <asjer94@live.com>
2023-11-05 17:12:53 +0100

    Adds exposed port to Dockerfile

--- sample-apps/jaxrs-json/src/test/java/org/example/app/JaxrsJsonIT.java
+++ sample-apps/jaxrs-json/src/test/java/org/example/app/JaxrsJsonIT.java
@@ -40,7 +40,6 @@ public class JaxrsJsonIT {
     
     @Container
     public static ApplicationContainer app = new ApplicationContainer()
-                    .withExposedPorts(9080)
                     .withAppContextRoot("/myservice")
                     .withReadinessPath("/myservice/app/people");
     
